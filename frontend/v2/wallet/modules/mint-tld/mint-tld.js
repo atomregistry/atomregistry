3d6ab3e404ed0ea2ff40e8069974d7cd5bd8fb5f
malkovsky <279437833+malkowsky@users.noreply.github.com>
2026-05-20 12:45:19 +0100

    Delete frontend/v2 directory
    i found bug and fix code! i will upload new one

--- frontend/v2/wallet/modules/mint-tld/mint-tld.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-window.ArViewInit = window.ArViewInit || {};
-
-window.ArViewInit['mint_tld'] = function () {
-  if (typeof window.initMintTldPage === 'function') window.initMintTldPage();
-};
